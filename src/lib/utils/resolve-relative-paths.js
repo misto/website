a4e6290c290ab220237a83ab06ddb130250e946a
Julian <julian@gitpod.io>
2022-07-11 16:11:32 +0000

    feat: resolve latest

--- src/lib/utils/resolve-relative-paths.js
+++ src/lib/utils/resolve-relative-paths.js
@@ -6,6 +6,8 @@ export default () => (tree, vFile) => {
       // resolve "index" in relative paths
       if (
         node.properties.href.includes("index") &&
+        !vFile.data.fm.filepath.includes("helm") &&
+        vFile.data.fm.filepath.includes("docs") &&
         node.properties.href.startsWith(".")
       ) {
         const version = vFile.data.fm.filepath
@@ -14,6 +16,22 @@ export default () => (tree, vFile) => {
           .reverse()
           .pop();
         const newPath = node.properties.href.replace("index", `../${version}`);
+        node.properties.href = newPath;
+      }
+
+      //resolve "latest" in relative paths
+      if (
+        node.properties.href.includes("latest") &&
+        !vFile.data.fm.filepath.includes("helm") &&
+        vFile.data.fm.filepath.includes("docs") &&
+        node.properties.href.startsWith(".")
+      ) {
+        const version = vFile.data.fm.filepath
+          .split("/")
+          .slice(-2)
+          .reverse()
+          .pop();
+        const newPath = node.properties.href.replace("latest", `${version}`);
         node.properties.href = newPath;
       }
     }
