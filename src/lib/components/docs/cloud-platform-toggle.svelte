e3155c6cf1030495612baff4515dbd70c5725b3f
Filip Troníček <filip@gitpod.io>
2022-07-12 17:44:41 +0000

    Adapt accessibiltiy updates for the Cloud Platform toggle

--- src/lib/components/docs/cloud-platform-toggle.svelte
+++ src/lib/components/docs/cloud-platform-toggle.svelte
@@ -29,7 +29,65 @@
 
   const clickHandler = (tabValue: number) => () => (activeValue = tabValue);
 
-  export let id = "cloud-platform-toggle";
+  let ariaIds: any = { tab: {}, tabpanel: {} };
+
+  if (!globalThis.counter) {
+    globalThis.counter = { tab: 1, tabpanel: 1 };
+  }
+
+  const getUnusedId = (() => {
+    //@ts-ignore
+    let { counter } = globalThis;
+    return (name: string, role: "tab" | "tabpanel") => {
+      const theId = `${role}-${counter[role]++}`;
+      ariaIds[role][name] = theId;
+      return theId;
+    };
+  })();
+
+  const switchableIndexes = items
+    .filter((item) => Object.keys($$slots).includes(item.slotName))
+    .map((item) => item.value);
+
+  const switchHandler = (e: KeyboardEvent) => {
+    const currentIndex = switchableIndexes.indexOf(activeValue);
+    //@ts-ignore
+    const siblings = e.target.parentElement.children;
+    switch (e.code) {
+      case "ArrowRight":
+        {
+          e.preventDefault();
+          const willOverflow = currentIndex === switchableIndexes.length - 1;
+          activeValue = willOverflow
+            ? switchableIndexes[0]
+            : switchableIndexes[currentIndex + 1];
+          siblings[willOverflow ? 0 : currentIndex + 1].focus();
+        }
+        break;
+      case "ArrowLeft":
+        {
+          e.preventDefault();
+          const willOverflow = currentIndex === 0;
+          activeValue = willOverflow
+            ? switchableIndexes[switchableIndexes.length - 1]
+            : switchableIndexes[currentIndex - 1];
+          siblings[
+            willOverflow ? switchableIndexes.length - 1 : currentIndex - 1
+          ].focus();
+        }
+        break;
+      case "Home":
+        e.preventDefault();
+        activeValue = switchableIndexes[0];
+        break;
+      case "End":
+        e.preventDefault();
+        activeValue = switchableIndexes[switchableIndexes.length - 1];
+        break;
+    }
+  };
+
+  export let id = "ide-toggle";
 
   export let open = false;
 </script>
@@ -48,10 +106,23 @@
   <summary class="text-p-medium">Cloud provider specific instructions</summary>
 
   <div class="my-8 mt-0">
-    <ul class="flex flex-wrap !pl-0 !mb-0">
+    <ul
+      class="flex flex-wrap !pl-0 !mb-0"
+      role="tablist"
+      on:keydown={switchHandler}
+    >
       {#each items as item}
         {#if Object.keys($$slots).includes(item.slotName) && !item.hidden}
-          <li class="!before:hidden">
+          <li
+            class="!before:hidden"
+            role="tab"
+            aria-selected={item.value === activeValue}
+            aria-controls={ariaIds.tabpanel[item.slotName]}
+            tabindex={item.value === activeValue ? 0 : -1}
+            on:click={clickHandler(item.value)}
+            on:focus={clickHandler(item.value)}
+            id={getUnusedId(item.slotName, "tab")}
+          >
             <span
               class="rounded-t-2xl cursor-pointer px-4 py-2 hidden md:block {activeValue ===
               item.value
@@ -71,25 +142,43 @@
       {/each}
     </ul>
     {#if $$slots.gcp}
-      {#if activeValue === 1}
-        <div class="box bg-white dark:bg-card">
-          <slot name="gcp" />
-        </div>
-      {/if}
+      <article
+        class={`box bg-white dark:bg-card ${activeValue !== 1 ? "hidden" : ""}`}
+        {...activeValue !== 1
+          ? { hidden: true, "aria-hidden": "true", tabindex: -1 }
+          : { hidden: false, "aria-hidden": "false", tabindex: 0 }}
+        aria-labelledby={ariaIds?.tab?.gcp}
+        id={getUnusedId("gcp", "tabpanel")}
+        role="tabpanel"
+      >
+        <slot name="gcp" />
+      </article>
     {/if}
     {#if $$slots.aws}
-      {#if activeValue === 2}
-        <div class="box bg-white dark:bg-card">
-          <slot name="aws" />
-        </div>
-      {/if}
+      <article
+        class={`box bg-white dark:bg-card ${activeValue !== 2 ? "hidden" : ""}`}
+        {...activeValue !== 2
+          ? { hidden: true, "aria-hidden": "true", tabindex: -1 }
+          : { hidden: false, "aria-hidden": "false", tabindex: 0 }}
+        aria-labelledby={ariaIds?.tab?.aws}
+        id={getUnusedId("aws", "tabpanel")}
+        role="tabpanel"
+      >
+        <slot name="aws" />
+      </article>
     {/if}
     {#if $$slots.azure}
-      {#if activeValue === 3}
-        <div class="box bg-white dark:bg-card">
-          <slot name="azure" />
-        </div>
-      {/if}
+      <article
+        class={`box bg-white dark:bg-card ${activeValue !== 3 ? "hidden" : ""}`}
+        {...activeValue !== 3
+          ? { hidden: true, "aria-hidden": "true", tabindex: -1 }
+          : { hidden: false, "aria-hidden": "false", tabindex: 0 }}
+        aria-labelledby={ariaIds?.tab?.azure}
+        id={getUnusedId("azure", "tabpanel")}
+        role="tabpanel"
+      >
+        <slot name="azure" />
+      </article>
     {/if}
   </div>
 </details>
