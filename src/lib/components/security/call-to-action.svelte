7736eabbddabef09d9e339c74055634c4678951c
MircoatGitpod <mirco@gitpod.io>
2022-07-13 14:28:18 +0000

    initial draft

--- src/lib/components/security/call-to-action.svelte
+++ src/lib/components/security/call-to-action.svelte
@@ -1,5 +1,9 @@
 <script>
-  import { reportAction, thanksAction } from "$lib/contents/security";
+  import {
+    reportAction,
+    thanksAction,
+    assessmentAction,
+  } from "$lib/contents/security";
   import Section from "../section.svelte";
   import Action from "./action.svelte";
 </script>
@@ -8,7 +12,7 @@
   <div
     class="flex flex-col md:flex-row items-center md:items-start justify-between max-w-3xl mx-auto space-y-medium md:space-y-0"
   >
-    {#each [thanksAction, reportAction] as action}
+    {#each [thanksAction, reportAction, assessmentAction] as action}
       <Action {action} />
     {/each}
   </div>
